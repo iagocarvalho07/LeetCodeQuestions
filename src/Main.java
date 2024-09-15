f5e1941903ce6273d244736ac629e41a485ecaae
iagocarvalho <71970033+iagocarvalho07@users.noreply.github.com>
2024-09-15 08:08:38 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import Easy.TowSun;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int [] arr = {2,7,11,15};
+        TowSun as = new TowSun();
+        System.out.println(Arrays.toString(as.twoSum(arr, 8)));  
+    }
+}
